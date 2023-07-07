ab0a603dba9ad25e3acd9cff5009f6c472d35dc9
defghi <IanMylesConroy@Gmail.com>
2023-07-06 17:51:53 -0700

    added code for playRound, playerSelection, computerSelection; needs work

--- repos/rockpaperscissors/javascript.js
+++ repos/rockpaperscissors/javascript.js
@@ -1,6 +1,57 @@
-function computerPlay() {
+function getComputerChoice() {
 const options = ["rock", "paper", "scissors"];
 const randomIndex = Math.floor(Math.random() * options.length);
 return options[randomIndex];
 }
-const computerChoice = computerPlay();
+const computerSelection = getComputerChoice();
+
+const rockButton = document.getElementById("rockbutton");
+const paperButton = document.getElementById("paperbutton");
+const scissorsButton = document.getElementById("scissorsbutton");
+
+rockButton.addEventListener("click", function() {
+    playRound("rock");
+})
+
+paperButton.addEventListener("click", function() {
+    playRound("paper");
+})
+
+scissorsButton.addEventListener("click", function() {
+    playRound("scissors");
+})
+
+function playRound(playerSelection, computerSelection) {
+    let post = "";
+
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            post = "Paper wins. Git gud, n00b.";
+        } else if (computerSelection ==="scissors") {
+            post = "You won. That sure \"rocks\" eh?";
+        } else {
+            post = "Tie. You both rocked out."
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            post = "Scissors won. Git gud, n00b."; 
+        } else if (computerSelection === "rock") {
+            post = "You won because somehow, in this game, paper>rock.";
+            } else {
+                post = "Tie. Neither of you could cut it."
+            }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            post = "You got rocked. Git gud, n00b.";
+        } else if (computerSelection === "paper") {
+            post = "You won. You made the cut.";
+        } else {
+            post = "Tie. Neither of you made the cut.";
+        }
+    }
+    return post;
+}
+
+
+    
+
