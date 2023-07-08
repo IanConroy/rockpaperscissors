cfb2a5070e47ad11993ebf4e9364ef5abc3c2806
defghi <IanMylesConroy@Gmail.com>
2023-07-08 09:23:33 -0700

    added scores, styled buttons, still troubleshooting playerSelection/playRound

--- repos/rockpaperscissors/javascript.js
+++ repos/rockpaperscissors/javascript.js
@@ -1,21 +1,26 @@
+let playerScore = 0;
+let computerScore = 0;
+let options = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
-const options = ["rock","paper","scissors"];
 const randomIndex = Math.floor(Math.random() * options.length);
 return options[randomIndex];
 }
 
 const computerSelection = getComputerChoice();
 
-
-const playerSelection = ["rock","paper","scissors"]
-
+function playerSelection(options) {
+rockButton.getElementById("rockbutton").onClick;
 rockButton.addEventListener("click", playRound("rock"));
 paperButton.addEventListener("click", playRound("paper"));
 scissorsButton.addEventListener("click", playRound("scissors"));
+}
+
+
 
 function playRound(playerSelection, computerSelection) {
     let postresult = "";
-
+    
     if (playerSelection === computerSelection) {
         postresult = "Draw! Everyone lives another day...";
     } else if (
@@ -24,14 +29,19 @@ function playRound(playerSelection, computerSelection) {
         (userSelection === "scissors" && computerChoice === "paper")
     ) {
         postresult = "You Win! Take that, robo!";
+        playerScore++;
     } else {
         postresult = "You lose! Pew pew! ZING!!!";
+        computerScore++;
     }
 
 document.getElementById("results").innerHTML = `
 <p>You chose: <strong>${playerSelection}</strong></p>
 <p>Computer chose: <strong>${computerSelection}</strong></p>
-<p>${postresult}</p>
+<p class="postresult">${postresult}</p>
 `;
+
+document.getElementById("playerScore").textContent = playerScore;
+document.getElementById("computerScore").textContent = computerScore;
 }
 
