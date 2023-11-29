bafab4bf6ebb4e0c7f96fea0f4a5ba9e2ea10349
IanConroy <IanMylesConroy@Gmail.com>
2023-11-28 20:23:43 -0800

    rebuilding javascript; previous code seems lost. cleaning code; fixed camelCase

--- repos/rockpaperscissors/javascript.js
+++ repos/rockpaperscissors/javascript.js
@@ -4,44 +4,26 @@ let options = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
 const randomIndex = Math.floor(Math.random() * options.length);
-return options[randomIndex];
+return options;
 }
 
 const computerSelection = getComputerChoice();
 
-function playerSelection(options) {
-rockButton.getElementById("rockbutton").onClick;
-rockButton.addEventListener("click", playRound("rock"));
-paperButton.addEventListener("click", playRound("paper"));
-scissorsButton.addEventListener("click", playRound("scissors"));
-}
-
-
-
 function playRound(playerSelection, computerSelection) {
-    let postresult = "";
+    let postResult = "";
     
     if (playerSelection === computerSelection) {
-        postresult = "Draw! Everyone lives another day...";
+        postResult = "Draw! Everyone lives another day...";
     } else if (
         (playerSelection === "rock" && computerChoice === "scissors") ||
         (playerSelection === "paper" && computerChoice === "rock") ||
         (playerSelection === "scissors" && computerChoice === "paper")
     ) {
-        postresult = "You Win! Take that, robo!";
+        postResult = "You Win! Take that, robo!";
         playerScore++;
     } else {
-        postresult = "You lose! Pew pew! ZING!!!";
+        postResult = "You lose! Pew pew! ZING!!!";
         computerScore++;
     }
-
-document.getElementById("results").innerHTML = `
-<p>You chose: <strong>${playerSelection}</strong></p>
-<p>Computer chose: <strong>${computerSelection}</strong></p>
-<p class="postresult">${postresult}</p>
-`;
-
-document.getElementById("playerScore").textContent = playerScore;
-document.getElementById("computerScore").textContent = computerScore;
 }
 
