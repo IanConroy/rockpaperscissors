b9ba6c3bace872e93352aec73cf5ed003fbc6a2e
IanConroy <IanMylesConroy@Gmail.com>
2023-11-28 20:31:58 -0800

    found functioning legacy code; remade

--- repos/rockpaperscissors/javascript.js
+++ repos/rockpaperscissors/javascript.js
@@ -1,29 +1,47 @@
-let playerScore = 0;
-let computerScore = 0;
-let options = ["rock", "paper", "scissors"];
+let options = ["rock", "paper", "scissors"]
 
 function getComputerChoice() {
-const randomIndex = Math.floor(Math.random() * options.length);
-return options;
+let randomizer = Math.floor(Math.random() * options.length);
+let computerOpt = options[randomizer];
+return computerOpt;
 }
 
-const computerSelection = getComputerChoice();
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
 
-function playRound(playerSelection, computerSelection) {
-    let postResult = "";
-    
-    if (playerSelection === computerSelection) {
-        postResult = "Draw! Everyone lives another day...";
-    } else if (
-        (playerSelection === "rock" && computerChoice === "scissors") ||
-        (playerSelection === "paper" && computerChoice === "rock") ||
-        (playerSelection === "scissors" && computerChoice === "paper")
-    ) {
-        postResult = "You Win! Take that, robo!";
-        playerScore++;
-    } else {
-        postResult = "You lose! Pew pew! ZING!!!";
+    for (let round = 1; round <= 5; round++) {
+    let playerSelection = prompt("What is your choice of weaponry? Rock, paper or scissors?");
+    let computerSelection = getComputerChoice();
+    let result = playRound(playerSelection, computerSelection);
+    console.log(result);
+    if (result.includes("win")) {
+        playerScore++; 
+    } else if (result.includes("lose")) {
         computerScore++;
     }
+    }
+    if (playerScore > computerScore) {
+        console.log("Well done. You have vanquished robo...for now.");
+    } else if (playerScore < computerScore) {
+        console.log("YOU LOSE. Pew pew! ZING!!!!")
+    } else {
+        console.log("Draw...");
+    }
 }
-
+function playRound(playerSelection, computerSelection) {
+    const player = playerSelection.toLowerCase();
+    const computer = computerSelection.toLowerCase();
+    if (player === computer) {
+        return "It's a tie!";
+    }
+    if (
+        (player === "rock" && computer === "scissors") ||
+        (player === "paper" && computer === "rock") ||
+        (player === "scissors" && computer === "paper") 
+    ) {
+        return `You win! ${player} beats ${computer}!`;
+    } else {
+    return `You lose! ${computer} beats ${player}!`;
+ }    
+}
