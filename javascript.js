dd5651adf252b02655a229b346f9bfe513e268da
IanConroy <IanMylesConroy@Gmail.com>
2024-01-22 15:13:51 -0800

    functional! minor cleanup/adjustments preferred

--- javascript.js
+++ javascript.js
@@ -6,7 +6,13 @@ function getComputerChoice() {
     return computerOpt.toLowerCase();
 }
 
- function game() {
+let playerScore = 0;
+let computerScore = 0;
+
+document.getElementById("pScore").textContent = "Player Score: " + playerScore;
+document.getElementById("cScore").textContent = "Computer Score: " + computerScore;
+
+ /* function game() {
     let playerScore = 0;
     let computerScore = 0;
     for (let round = 1; round <= 5; round++) {
@@ -29,17 +35,73 @@ function getComputerChoice() {
             } else {
                 console.log("Draw...");
             } 
-            function playRound(player, computer) {
-                if (player === computer) {
-                    return `"tie. you both chose ${player}"`;
-                    
-                } else if (
-                    (player === "rock" && computer === "scissors") ||
-                    (player === "paper" && computer === "rock") ||
-                    (player === "scissors" && computer === "paper")
+*/
+
+/*      !!!ALTERNATIVE IF-ELSE STATEMENT!!!
+        function playRound(playerSelection) {
+            let player = playerSelection.toLowerCase();
+            computerSelection = getComputerChoice();
+            let computer = computerSelection.toLowerCase();
+            let results = document.getElementById("gameResults");    
+            if (player === computer) {
+                results.textContent = `tie. you both chose ${player}`;
+            } else if (
+                (player === "rock" && computer === "scissors") ||
+                (player === "paper" && computer === "rock") ||
+                (player === "scissors" && computer === "paper")
                 ) {
-                    return `"you win! ${player} beats ${computer}"`;
-                } else { return `"you lose. ${computer} beats ${player}"`;
-                    }
+                playerScore++;
+                results.textContent = `you win! ${player} beats ${computer}`;
+            } else { 
+                computerScore++;
+                results.textContent = `you lose. ${computer} beats ${player}`;
+        }         
+        document.getElementById("pScore").innerHTML = "Player Score: " + playerScore;
+        document.getElementById("cScore").innerHTML = "Computer Score: " + computerScore;
+}
+*/
+
+function playRound(playerSelection) {
+    let player = playerSelection.toLowerCase();
+    computerSelection = getComputerChoice();
+    let computer = computerSelection.toLowerCase();
+    let results = document.getElementById("gameResults");
+    let nameWinner = document.getElementById("nameWinner");
+    if (playerScore === 5) {
+        nameWinner.textContent = "you have vanquished AI...";
+        return "but they live in your head rent free...";
+    }
+    if (computerScore === 5) {
+        nameWinner.textContent = "uh oh. AI won. now comes the apocalypse";
+        return "you need a time machine";
+    }
+            switch (`${player}-${computer}`) {      
+            case "rock-scissors":
+            case "paper-rock":
+            case "scissors-paper":
+            playerScore++;
+            results.textContent = `you win. ${player} beats ${computer}`;
+            break;
+            case "rock-paper":
+            case "paper-scissors":
+            case "scissors-rock":
+            results.textContent = `you lose. ${computer} beats ${player}`;
+            computerScore++;
+            break;
+            default:
+            results.textContent = `tie. you both chose ${player}`
             }
-        }
+            document.getElementById("pScore").textContent = "Player Score: " + playerScore;
+            document.getElementById("cScore").textContent = "Computer Score: " + computerScore;
+            
+    }
+
+    document.getElementById("rockButton").addEventListener("click", function() {
+        playRound("rock");
+    });
+    document.getElementById("paperButton").addEventListener("click", function() {
+        playRound("paper");
+    });
+    document.getElementById("scissorsButton").addEventListener("click", function() {
+        playRound("scissors");
+    });
